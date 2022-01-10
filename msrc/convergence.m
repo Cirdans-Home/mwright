83a9880e3c449393f8d208fb6be2cfee7df6608b
Cirdans-Home <f.durastante@na.iac.cnr.it>
2022-01-10 16:07:48 +0100

    Convergence for the Mainardi function

--- msrc/convergence.m
+++ msrc/convergence.m
@@ -5,7 +5,7 @@
 
 clear; clc; close all;
 
-M = 100;
+M = 25;
 x = linspace(-5,5,M).';
 
 %% \nu = 1/2
@@ -60,4 +60,31 @@ end
 
 legend;
 title('$\lambda = -1/3, \; \mu = 2/3$','Interpreter','latex')
+set(gca,'YScale','log');
+
+%% \nu = 0
+true = @(z) exp(-abs(z));
+nu = 0;
+w = zeros(M,1);
+for i=1:M
+   [w(i),N] = mwright(x(i),1,-nu,1-nu); 
+end
+
+Nvalues = unique(round(linspace(2,N,4)));
+
+for N = Nvalues
+    w = zeros(M,1);
+    for i=1:M
+        w(i) = mwright(x(i),1,-nu,1-nu,N);
+    end
+    
+    figure(3)
+    hold on
+    semilogy(x,abs(w-true(x))./true(x),'DisplayName',sprintf('N = %d',N),'LineWidth',2)
+    hold off
+    
+end
+
+legend;
+title('$\lambda = 0, \; \mu = 0$','Interpreter','latex')
 set(gca,'YScale','log');
